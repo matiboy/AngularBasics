894978ebc61fe6ee88cedc5128e35dae99185366
Red Ape Solutions <redapesolutions@Reds-MacBook-Pro.local>
2013-10-11 10:53:43 +0800

    Adding a dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('AngularBasicsApp', [])
+angular.module('AngularBasicsApp', ['ngCookies'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('AngularBasicsApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $cookies) {
+  	console.debug($cookies);
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',

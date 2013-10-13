4576dbf44c7d536a4f7fde61d5c4e4900b7f3b54
Mathieu <mathieu@redapesolutions.com>
2013-10-13 16:43:17 +0800

    Show two way data-binding and filtering (without using Angular.js filters)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,5 +8,24 @@ angular.module('AngularBasicsApp')
     	console.debug('Received parts for address: ', parts);
     	addressParts = parts;
     	$scope.addressParts = parts;
+    	// Create an array of unique types of addresses, by adding types from each result.
+    	// Using union ensure unicity
+    	var types = [];
+    	_.each(parts, function(part) {
+    		types = _.union(types, part.types);
+    	});
+    	$scope.addressTypes = types;
+
+    	// Listen to changes on filter and filter parts accordingly
+    	// $watch will be discussed at a later stage
+    	$scope.$watch('addressFilter', function(nv, ov){
+    		console.debug('Address filter changed from ' + ov + ' to ' + nv);
+    		// filter parts
+    		if(nv){
+    			$scope.addressParts = _.filter(addressParts, function(part) {
+	    			return _.contains(part.types,nv);
+	    		});
+    		}
+    	});
     });
   });

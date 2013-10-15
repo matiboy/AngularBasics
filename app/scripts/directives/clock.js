b530e46f126689e17ddc1ae9200b8661f5ea20aa
Mathieu <mathieu@redapesolutions.com>
2013-10-15 17:47:05 +0800

    Make the clock work

--- app/scripts/directives/clock.js
+++ app/scripts/directives/clock.js
@@ -6,10 +6,18 @@ angular.module('AngularBasicsApp')
       template: '<div><span ng-bind="hour"></span><span ng-bind="separator"></span><span ng-bind="minute"></span><span ng-bind="separator"></span><span ng-bind="second"></span></div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.hour = 3;
-        scope.minute = 35;
-        scope.second = 22;
-        scope.separator = ':';
+        scope.separator = attrs.separator || ':';
+        var period = attrs.period || 1000;
+        // Change to (new?) $interval service
+        setInterval(function() {
+          var now = moment();
+          scope.hour = now.format('HH');
+          scope.minute = now.format('mm');
+          scope.second = now.format('ss');
+          try {
+            scope.$digest();
+          } catch(e) {}
+        }, period);
       }
     };
   });

5adb45f2bd8751fc58b7ce8b7c5bbf546f1d0850
Mathieu <mathieu@redapesolutions.com>
2013-10-15 17:54:53 +0800

    Add time zone and creation of new scope

--- app/scripts/directives/clock.js
+++ app/scripts/directives/clock.js
@@ -5,12 +5,14 @@ angular.module('AngularBasicsApp')
     return {
       template: '<div><span ng-bind="hour"></span><span ng-bind="separator"></span><span ng-bind="minute"></span><span ng-bind="separator"></span><span ng-bind="second"></span></div>',
       restrict: 'E',
+      scope: true,
       link: function postLink(scope, element, attrs) {
         scope.separator = attrs.separator || ':';
         var period = attrs.period || 1000;
+        var tz = attrs.tz || 0;
         // Change to (new?) $interval service
         setInterval(function() {
-          var now = moment();
+          var now = moment().add('h', tz);
           scope.hour = now.format('HH');
           scope.minute = now.format('mm');
           scope.second = now.format('ss');

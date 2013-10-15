262ee43348d5dafeabfb1a489df0033708e2ef82
Mathieu <mathieu@redapesolutions.com>
2013-10-15 16:28:47 +0800

    Tabs example

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('AngularBasicsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope, $sce) {
+    this.trustedTabs = [];
+    this.tabs = [
+      {
+        title: 'ABC',
+        content: 'A<br>B<br>C'
+      },
+      {
+        title: 'EDF',
+        content: 'D<br>E<br>F'
+      }
     ];
+    // Un-protect model
+    this.jkl = $sce.trustAsHtml('J<br>K<br>L');
+
+    this.addTab = function() {
+      this.trustedTabs.push({
+        title: this.tab.title,
+        content: $sce.trustAsHtml(this.tab.content)
+      });
+    }
+
+    this.alertMe(_.partial(window.alert, 'You ve selected the select tab'));
   });
